9180ae947f2b212deded1cc7e410ef3e636e02fc
haseen20 <126189491+haseen20@users.noreply.github.com>
2024-09-26 13:08:51 -0400

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "c48ff8912aca7ec88116502f57dc0457"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "3b1cdfe36114428fa9a30a110f7c7588"
